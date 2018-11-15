d69853ab747db4f0a28510035209aad349a5ccce
Minh Pham <mpham@naver.com>
2018-11-15 13:10:14 +0100

    Finish Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Tim Raue',
+    address:      'Rudi-Dutschke-Str. 26, 10969 Berlin',
+    category:     'french',
+    phone_number: '030 25937930'
+  },
+  {
+    name:         'Entrecote',
+    address:      'Schutzenstr. 5, 10117 Berlin',
+    category:     'french',
+    phone_number: '030 20165496'
+  },
+  {
+    name:         'Ristorante Sale e Tabacchi',
+    address:      'Rudi-Dutschke-Str. 23, 10969 Berlin',
+    category:     'italian',
+    phone_number: '030 2521155'
+  },
+  {
+    name:         'Goodtime',
+    address:      'Hausvogteiplatz 11, 10117 Berlin',
+    category:     'chinese',
+    phone_number: '030 20074870'
+  },
+  {
+    name:         'Ishin',
+    address:      'Charlottenstr. 16, 10117 Berlin',
+    category:     'japanese',
+    phone_number: '030 60500172'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
